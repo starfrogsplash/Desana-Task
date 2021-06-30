2d232f2c905098214f1f43fea41def924f0c23b4
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-06-30 22:51:34 +0100

    updated code with feedback

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { OpeningTimes, Space } from "./types";
-import { format } from "date-fns";
+import { format, addDays } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
 /**
@@ -16,36 +16,28 @@ export const fetchAvailability = (
 ): Record<string, OpeningTimes> => {
   const dateFormatted = format(now, "yyyy-MM-dd").toString();
 
-  const zero = 0;
-  const fifteen = 15;
-  const thirty = 30;
-  const fourtyFive = 45;
+  const addedDays = addDays(now, numberOfDays);
 
-  const utcDate = utcToZonedTime(now, space.timeZone);
+  const utcDate = utcToZonedTime(addedDays, space.timeZone);
 
-  const convertedTime = format(utcDate, "yyyy-mm-dd k-mm")
-    .toString()
-    .split(" ")[1];
+  const convertedTime = format(utcDate, "k-mm").toString();
+
+  const isoDay = format(utcDate, "i").toString();
 
   let convertedHour = parseInt(convertedTime.replace(/^0+/, "").split("-")[0]);
   const convertedMinute = parseInt(
     convertedTime.replace(/^0+/, "").split("-")[1]
   );
 
-  let minuteWithNotice = convertedMinute + space.minimumNotice;
+  const minuteWithNotice = convertedMinute + space.minimumNotice;
 
-  if (minuteWithNotice > zero && minuteWithNotice < fifteen)
-    minuteWithNotice = fifteen;
-  if (minuteWithNotice > fifteen && minuteWithNotice < thirty)
-    minuteWithNotice = thirty;
-  if (minuteWithNotice > thirty && minuteWithNotice < fourtyFive)
-    minuteWithNotice = fourtyFive;
-  if (minuteWithNotice > fourtyFive) {
-    minuteWithNotice = zero;
+  const roundedMinute = (Math.ceil(minuteWithNotice / 15) * 15) % 60;
+
+  if (roundedMinute === 0) {
     convertedHour++;
   }
 
-  const selectedDay = Object.entries(space)[0][1][numberOfDays];
+  const selectedDay = Object.entries(space)[0][1][isoDay];
 
   const openHour = selectedDay.open.hour;
   const openMinutes = selectedDay.open.minute;
@@ -73,7 +65,7 @@ export const fetchAvailability = (
       [dateFormatted]: {
         open: {
           hour: convertedHour,
-          minute: minuteWithNotice,
+          minute: roundedMinute,
         },
         close: {
           hour: closeHour,
